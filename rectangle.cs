50579eb0ca52e74019cc40cbb1607b2acc78b6c8
Vatsal253 <vatsaljain020503@gmail.com>
2025-01-19 16:33:26 +0530

    added solutions

---
+++ rectangle.cs
@@ -0,0 +1,13 @@
+using System;
+	class rectangle
+	{
+	static void Main()
+	{
+	Console.WriteLine("Enter the length :");
+	double length = Convert.ToDouble(Console.ReadLine());
+	Console.WriteLine("Enter the width :");
+	double width = Convert.ToDouble(Console.ReadLine());
+	double perimeter = 2*(length + width);
+	Console.WriteLine("The perimeter of rectangle is: "+perimeter);
+	}
+	}
